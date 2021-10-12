6df8d35d3b79027fc47aa94af9dd0954ec5ed216
Igor-Chernov55 <79840255+Igor-Chernov55@users.noreply.github.com>
2021-10-12 12:03:56 +0300

    Delete my-app directory

--- my-app/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { Header } from './Header';
-
-
-window.addEventListener('load', () => {
-    ReactDom.render(<Header />, document.getElementById('react_root'))
-})
-
