861c2e17cfe42186667949bcb03a6e47d6c0e82c
Igor-Chernov55 <igorek_chernov_1998@mail.ru>
2021-11-10 14:49:54 +0300

    added test comment

--- src/components/Main/Profile/Posts/ProfileContainer.tsx
+++ src/components/Main/Profile/Posts/ProfileContainer.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React, {Dispatch} from 'react'
 import Dialogs from "../../../Dialogs/Dialogs";
-import {Dispatch} from "react";
-import {ActionsPostsReducerType, addPostAC, changePostAC} from "../../../StateComponents/PostsReducer";
 import {DialogsType, MessageType} from "../../../StateComponents/State";
+import {ActionsPostsReducerType, addPostAC, changePostAC} from "../../../StateComponents/PostsReducer";
 
-type PropsType = {
-    dispatch: Dispatch<ActionsPostsReducerType>
+type ProfileTypes = {
     newMessage: string
-    dialogs: DialogsType[]
-    message: MessageType[]
+    dialogs: Array<DialogsType>
+    message: Array<MessageType>
+    dispatch: Dispatch<ActionsPostsReducerType>
 }
 
-const ProfileContainer = (props: PropsType) => {
+const ProfileContainer = (props: ProfileTypes) => {
 
     const addPost = () => {
         props.dispatch(addPostAC())
@@ -21,11 +20,10 @@ const ProfileContainer = (props: PropsType) => {
         props.dispatch(changePostAC(text))
     }
 
-
     return (
         <Dialogs
-            newMessage={props.newMessage}
             dialogs={props.dialogs}
+            newMessage={props.newMessage}
             message={props.message}
             addPost={addPost}
             onChange={onChange}
